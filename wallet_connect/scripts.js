493df87e31cb07e7509873c62c71037f68f49886
Minsung Kim <akatjd11@gmail.com>
2023-01-15 22:19:32 +0900

    Update scripts.js

--- wallet_connect/scripts.js
+++ wallet_connect/scripts.js
@@ -50,7 +50,21 @@ const BLOCKCHAIN_EXPLORERS = {
 /**
  * ABI needed to interpret how to interact with the contract
  */
-const CONTRACT_ABI = [];
+const CONTRACT_ABI = [
+            {
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "nft_id",
+						"type": "uint256"
+					}
+				],
+				"name": "do_mine",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			}
+];
 
 // Functions
 // ========================================================
